95f98bb89967c5d174b5d686f3f3863cf27b3a62
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-08 17:23:10 -0500

    Feat: ejercicio 7 realizado

---
+++ clase6/Ejercicio7.java
@@ -0,0 +1,15 @@
+package clase6;
+
+public class Ejercicio7 {
+    /*
+        Muestra los números del 1 al 100 utilizando un bucle while.
+     */
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while(contador <= 100) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
